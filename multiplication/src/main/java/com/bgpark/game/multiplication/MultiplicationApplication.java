72a29660940a3a0dede010cefc2794ef6baec8ed
bgpark <bgpark82@gmail.com>
2020-12-25 22:03:41 +0900

    feat: Eureka 클라이언트 추가

--- multiplication/src/main/java/com/bgpark/game/multiplication/MultiplicationApplication.java
+++ multiplication/src/main/java/com/bgpark/game/multiplication/MultiplicationApplication.java
@@ -2,6 +2,7 @@ package com.bgpark.game.multiplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class MultiplicationApplication {
