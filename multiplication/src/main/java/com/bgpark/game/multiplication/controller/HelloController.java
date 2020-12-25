45978f920f22dc79d29699fbc31c5c040c1d7134
bgpark <bgpark82@gmail.com>
2020-12-26 00:06:52 +0900

    feat: docker-compose.yml 추가

--- multiplication/src/main/java/com/bgpark/game/multiplication/controller/HelloController.java
+++ multiplication/src/main/java/com/bgpark/game/multiplication/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.bgpark.game.multiplication.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- user/src/main/java/com/bgpark/game/user/service/HelloServiceProxy.java
+++ user/src/main/java/com/bgpark/game/user/service/HelloServiceProxy.java
@@ -3,7 +3,7 @@ package com.bgpark.game.user.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "multiplication", url = "${MULTIPLICATION_URL:localhost:8081}")
+@FeignClient(name = "multiplication")
 public interface HelloServiceProxy {
 
     @GetMapping("/")
